96d22022059b0c5f5155076b19a0f5f8d01a6fb6
ChristinaFrantsova <mob0964442737@gmail.com>
2022-10-07 09:48:39 +0300

    rerecorded repository with tasks

---
+++ src/js/02-lightbox.js
@@ -0,0 +1,27 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+// 1.Створення галереї
+const galleryImages = document.querySelector(".gallery");
+
+const itemOfGallery = galleryItems
+  .map(
+    (element) =>
+      `<a class="gallery__item" href=${element.original}>
+  <img class="gallery__image" src=${element.preview} alt=${element.description} />
+</a>`
+  )
+  .join(" ");
+console.log(itemOfGallery);
+
+galleryImages.insertAdjacentHTML("afterbegin", itemOfGallery);
+
+// Через «Options» - документацію додали
+// відображення підписів до зображень з атрибута та
+// час появлення підпису
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
